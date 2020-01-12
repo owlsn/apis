943d2453fce8c2727e5e76741d66b59a6ed75ee2
owlsn <helloworld.zouhao@gmail.com>
2020-01-12 22:46:51 +0800

    add base repositories

---
+++ src/repositories/base.go
@@ -0,0 +1,23 @@
+package repositories
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/owlsn/apis/src/datamodels"
+	"github.com/owlsn/apis/src/utils/database"
+)
+
+type baseRepository struct {
+	table interface{}
+}
+
+func Get(b *baseRepository){
+
+}
+
+func GetAll(b *baseRepository){
+
+}
+
+func PageList(b *baseRepository){
+	
+}
